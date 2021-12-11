123ab8924fa83f9a60745e47be30b0e46cf98563
levinz97 <levinzeng97@gmail.com>
2021-12-11 11:29:08 +0100

    add selectiveSearch in class preperaData, refactor prepare_data.py

--- test/test_prepare_data.py
+++ test/test_prepare_data.py
@@ -1,20 +1,20 @@
 import unittest
-from prepare_data import prepare_data
+from prepare_data import PrepareData
 import cv2
 
 
 class Test_prepare_data(unittest.TestCase):
     
     def test_get_contours(self):
-        pd = prepare_data()
+        pd = PrepareData()
         for i in range(0,31):
             filename = 'frame{}.png'.format(str(i*10))
             print('>>>>>>>>open file: '+filename)
             img = cv2.imread(filename)
             img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
-            img = pd.presegment_img(img, method='grabcut',display_result=False)
-            contours = pd.get_contours(img,display_result=False)
-            self.assertGreater(len(contours), 0)
+            img = pd.presegmentImg(img, method='grabcut',display_result=False)
+            contours = pd.getContours(img,display_result=False)
+            self.assertGreater(len(contours), -1)
 
 
 
