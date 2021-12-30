115677646d196e9fac6fb7b29f1e471da4414f79
levinz97 <levinzeng97@gmail.com>
2021-12-30 13:06:24 +0100

    add feat, ensemble method for better cls perf in svm

--- svm.py
+++ svm.py
@@ -1,9 +1,15 @@
 import json
 import numpy as np
 import warnings
+from scipy.sparse.construct import rand
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn import svm, metrics
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from scipy import stats
 
 
 class Data_hsv():
@@ -17,10 +23,11 @@ class Data_hsv():
             dictionary = json.load(input_file)
             input_file.close()
         number = len(dictionary)
-        #print(a)
+        # print(a)
 
-        material_list = ['r','m']
-        color_list = ['red', 'blue', 'cyan', 'green', 'gold', 'purple', 'yellow', 'gray', 'brown']
+        material_list = ['r', 'm']
+        color_list = ['red', 'blue', 'cyan', 'green',
+                      'gold', 'purple', 'yellow', 'gray', 'brown']
         label_dict = {}
         label_list = []
 
@@ -37,7 +44,7 @@ class Data_hsv():
                 label_dict[j]['hsv'].extend(dictionary[i][j]['hsv'])
                 label_dict[j]['rgb'].extend(dictionary[i][j]['rgb'])
 
-        #print(label_dict)
+        # print(label_dict)
         return label_dict
 
 
@@ -54,7 +61,8 @@ class Svm():
         elif class_label == 'm':
             return self.material
         else:
-            print('wrong input')
+            # print('wrong input')
+            return self.color_material
 
     def color_material(self):
         feature = []
@@ -95,25 +103,79 @@ class Svm():
         return feature, label
 
     def train(self, feature, label):
-        random_state = np.random.RandomState(0)
+        random_state = np.random.RandomState(np.random.randint(0, 1000))
+        # random_state = np.random.RandomState(0)
         # test_size is the proportion of the training set and can be modified
         X_train, X_test, y_train, y_test = train_test_split(feature, label, test_size=0.2, random_state=0)
         # Kernel function can choose 'rbf' Gaussian kernel or 'poly' polynomial kernel
         # Result of 'linear' kernel is very poor
-        model = OneVsRestClassifier(svm.SVC(kernel='poly', probability=True, random_state = random_state))
+
+        models = []
+        model = OneVsRestClassifier(svm.SVC(kernel='poly', probability=True, random_state=random_state))
+        models.append(model)
+        model  = RandomForestClassifier(n_estimators=80, random_state=random_state)
+        # models.append(model)#85
+        model  = RandomForestClassifier(n_estimators=120, random_state=random_state)
+        # models.append(model)#85
+        model1 = RandomForestClassifier(n_estimators=350, random_state=random_state)
+        models.append(model1)#86
+        model2 = MLPClassifier(solver='lbfgs', alpha=1e-6, hidden_layer_sizes=(5, 18, 10, 5), random_state=random_state)
+        # models.append(model2)
+        model3 = MLPClassifier(solver='sgd', activation='relu',learning_rate_init=0.0005, learning_rate='adaptive', alpha=1e-6, hidden_layer_sizes=(150), random_state=random_state)
+        # models.append(model3) # for m 
+        model4 = MLPClassifier(solver='adam', activation='relu', learning_rate_init=1e-3, alpha=1e-3,hidden_layer_sizes=(20,40,60,80), random_state=random_state)
+        models.append(model4)
+        model5 = MLPClassifier(solver='sgd', activation='relu',learning_rate='adaptive',shuffle=False, alpha=1e-6,hidden_layer_sizes=(150), batch_size=32, random_state=random_state)
+        # models.append(model5)
+        model6 = OneVsRestClassifier(svm.SVC(kernel='rbf', probability=True, random_state=random_state))
+        # models.append(model6)
+        assert len(models) > 0, "no model added!"
         print("[INFO] Successfully initialize a new model !")
         print("[INFO] Training the model…… ")
-        clt = model.fit(X_train, y_train)
-        print("[INFO] Model training completed !")
-        y_test_pred = clt.predict(X_test)
+
+        def ensembleLearning(model):
+            # pipe = make_pipeline(StandardScaler(),model)
+            pipe = model
+            pipe.fit(X_train, y_train)
+            print("[INFO] Model training completed !")
+            y_test_pred = pipe.predict(X_test)
+            return y_test_pred
+        y_test_pred = 0
+        cnt = 0
+        for m in models:
+            cnt += 1
+            y_p = ensembleLearning(m)
+            if isinstance(y_test_pred, int):
+                y_test_pred = y_p
+            else:
+                if np.max(y_p) != np.min(y_p):
+                    y_test_pred = np.vstack((y_test_pred, y_p))
+                    # if m == model5  or m == model4:
+                    #     y_test_pred = np.vstack((y_test_pred,y_p))
+                else:
+                    print(
+                        f"[{'warning'.upper()}] model Failed:\n{m} collapses\n with output{y_p}")
+
+        y_test_pred = y_test_pred.astype(np.uint8)
+        print(y_test_pred)
+        
+        if len(y_test_pred.shape) > 1:
+            y_test_pred = y_test_pred.transpose()
+            y_test_pred, _ = stats.mode(y_test_pred, axis=1)
+        print(y_test_pred.transpose())
+        print(f"GT:\n {y_test}")
         ov_acc = metrics.accuracy_score(y_test_pred, y_test)
+        # ov_acc = pipe.score(X_test, y_test)
         print("overall accuracy: %f" % (ov_acc))
         print("===========================================")
-        acc_for_each_class = metrics.precision_score(y_test, y_test_pred, average=None)
+        acc_for_each_class = metrics.precision_score(
+            y_test, y_test_pred, average=None)
         print("acc_for_each_class:\n", acc_for_each_class)
         print("===========================================")
         avg_acc = np.mean(acc_for_each_class)
         print("average accuracy:%f" % (avg_acc))
+        
+        return ov_acc
 
 
 def main():
@@ -124,13 +186,16 @@ def main():
     svm1 = Svm(label_dict)
     label = svm1.class_choice(class_label)
     feature, label = label()
-    print(feature)
-    print(label)
+    # print(feature)
+    # print(label)
     feature = np.array(feature)
     label = np.array(label)
-    svm1.train(feature, label)
+    acc_list = []
+    for i in range(100):
+        acc_list.append(svm1.train(feature, label))
+    print(f"\noverall average accuracy is {np.mean(acc_list)}")
+    print(f"standard deviation of accuracy is {np.std(acc_list)}")
+    
 
 if __name__ == "__main__":
     main()
-
-
