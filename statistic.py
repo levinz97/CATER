1b6a416ed7f74f5afd5090908fc5ba712a407dbf
Alaric423 <alarictong@gmail.com>
2022-01-04 09:14:11 +0100

    add shape

--- statistic.py
+++ statistic.py
@@ -3,9 +3,10 @@ import json
 
 class Statistic:
     def __init__(self):
-        material_list = ['r','m']
+        material_list = ['r', 'm']
         color_list = ['red', 'blue', 'cyan', 'green', 'gold', 'purple', 'yellow', 'gray', 'brown']
-        self.size_list = ['s', 'b', 'l']
+        self.size_list = ['s', 'm', 'l']
+        self.shape_list = ['cub', 'con', 'spl', 'sph', 'cyl']
         self.label_dict = {}
         self.name_list = []
         for i in color_list:
@@ -18,6 +19,7 @@ class Statistic:
                 self.label_dict[name]['length'] = []
                 self.label_dict[name]['center'] = []
                 self.label_dict[name]['size'] = []
+                self.label_dict[name]['shape'] = []
                 self.name_list.append(name)
 
     def data_log(self, avg_hsv, avg_rgb, area, length, center):
@@ -25,17 +27,21 @@ class Statistic:
         if label_input in self.name_list:
             size = input('input the size: ')
             if size in self.size_list:
-                self.label_dict[label_input]['hsv'].append(avg_hsv.tolist())
-                self.label_dict[label_input]['rgb'].append(avg_rgb.tolist())
-                self.label_dict[label_input]['area'].append(area)
-                self.label_dict[label_input]['length'].append(length)
-                self.label_dict[label_input]['center'].append(center)
-                self.label_dict[label_input]['size'].append(size)
+                shape = input('input the shape: ')
+                if shape in self.shape_list:
+                    self.label_dict[label_input]['hsv'].append(avg_hsv.tolist())
+                    self.label_dict[label_input]['rgb'].append(avg_rgb.tolist())
+                    self.label_dict[label_input]['area'].append(area)
+                    self.label_dict[label_input]['length'].append(length)
+                    self.label_dict[label_input]['center'].append(center)
+                    self.label_dict[label_input]['size'].append(size)
+                    self.label_dict[label_input]['shape'].append(shape)
 
-    def data_store(self):
+    def data_store(self, name):
         datalist = []
         datalist.append(self.label_dict)
-        color_json = json.dumps(datalist)
-        with open('./data', 'w', encoding='UTF-8') as output_file:
-            output_file.write(color_json)
+        data_json = json.dumps(datalist)
+        with open('./dataset/data{}'.format(name), 'w', encoding='UTF-8') as output_file:
+            output_file.write(data_json)
             output_file.close()
+        return self.label_dict
