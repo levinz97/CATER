a13e951d8aa30e649f48e84054bb24f229348c11
levinz97 <levinzeng97@gmail.com>
2022-01-07 22:07:20 +0100

    add feat, vis every predicted instance, use new dataset

--- demo.py
+++ demo.py
@@ -1,12 +1,13 @@
-from detectron2.data.build import build_detection_test_loader
-from detectron2.evaluation.evaluator import inference_on_dataset
+import warnings
 from utils import dispImg
 
-from detectron2.data.catalog import MetadataCatalog
-from detectron2.utils.logger import setup_logger
 from detectron2.config import get_cfg
+from detectron2.data.catalog import MetadataCatalog
+from detectron2.data.build import build_detection_test_loader
+from detectron2.utils.logger import setup_logger
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.evaluation.evaluator import inference_on_dataset
 from detectron2.evaluation import COCOEvaluator
 
 import cv2
@@ -17,22 +18,26 @@ import os
 if __name__ == "__main__":
     setup_logger()
     # register dataset
-    register_cater_dataset.register_dataset()
+    annotation_location = os.path.join('.', 'dataset', 'annotations','5200-5214.json')
+    img_folder = os.path.join('.', 'dataset', 'images','image')
+    register_cater_dataset.register_dataset(dataset_name='cater', annotations_location= annotation_location, image_folder= img_folder)
+    register_cater_dataset.register_dataset(dataset_name='cater_test')
     # set configuration file
     cfg = get_cfg()
     cfg.merge_from_file("detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 
     cfg.DATASETS.TRAIN = ("cater",)
     cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.DATALOADER.NUM_WORKERS = 4
 
     cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"
 
-    cfg.SOLVER.IMS_PER_BATCH = 1
-    cfg.SOLVER.MAX_ITER = (300)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9
+    cfg.SOLVER.IMS_PER_BATCH = 3
+    cfg.SOLVER.MAX_ITER = (1000)
+    cfg.SOLVER.BASE_LR = 0.01
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 270
     # number of ROI per image
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 50
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 20
 
     cfg.OUTPUT_DIR = "output"
     if input("print configurations? ") == 'y':
@@ -44,7 +49,7 @@ if __name__ == "__main__":
 
     if input("continue to evaluate? ") == 'y':
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-        cfg.DATASETS.TEST = ("cater",)
+        cfg.DATASETS.TEST = ("cater_test",)
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
         cfg.TEST.EVAL_PERIOD = 50
         model = trainer.build_model(cfg)
@@ -55,7 +60,8 @@ if __name__ == "__main__":
         # inference_on_dataset(trainer.model, val_loader, evaluator)
 
     if input("continue to predict? ") == 'y':
-        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        warnings.filterwarnings('ignore')
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
         predictor = DefaultPredictor(cfg)
         test_img = cv2.imread("test.png")
@@ -63,9 +69,13 @@ if __name__ == "__main__":
         cater_metadata = MetadataCatalog.get("cater")
         cater_metadata.set(evaluator_type="coco")
         test_img_pred = predictor(test_img)
-        v = Visualizer(test_img[:, :, ::-1],
-                    metadata=cater_metadata,
-                    instance_mode=ColorMode.IMAGE_BW)
-        v = v.draw_instance_predictions(test_img_pred["instances"].to("cpu"))
-        img = v.get_image()[:, :, ::-1]
-        dispImg("prediction", img, kill_window=False)
+        pred = test_img_pred["instances"].to("cpu")
+        print(f"there are totally {len(pred)} instances detected")
+        for item in range(len(pred)):
+            vis = Visualizer(test_img[:, :, ::-1],
+                        metadata=cater_metadata,
+                        instance_mode=ColorMode.IMAGE_BW)
+            v = vis.draw_instance_predictions(pred[item])
+            img = v.get_image()[:, :, ::-1]
+            dispImg("prediction", img, kill_window=True)
+            img = []
