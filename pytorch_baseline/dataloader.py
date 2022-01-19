33b779524ebe05f74f728a11ad37f5f081994641
levinz97 <levinzeng97@gmail.com>
2022-01-20 00:12:52 +0100

    refactor dataloader in pytorch_baseline

--- pytorch_baseline/dataloader.py
+++ pytorch_baseline/dataloader.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch.utils.data as data
 import torch
 from PIL import Image
@@ -14,7 +13,6 @@ class CaterDataloader(data.Dataset):
         assert os.path.isdir(image), image
         assert os.path.isfile(annotations), annotations
         self.image_dir = image
-        self.root = root
         self.coco = COCO(annotations)
         self.transforms = transforms
         self.ids = list(sorted(self.coco.imgs.keys()))
@@ -24,41 +22,45 @@ class CaterDataloader(data.Dataset):
         img_id = self.ids[index]
         anno_ids = self.coco.getAnnIds(imgIds=img_id)
         coco_annos = self.coco.loadAnns(anno_ids)
-        path = self.coco.loadImgs(img_id)[0]['file_name']
-        img = Image.open(os.path.join(self.root, self.image_dir, path))
+        img_path = self.coco.loadImgs(img_id)[0]['file_name']
+        img = Image.open(os.path.join(self.image_dir, img_path))
+        img = transforms.ToTensor()(img)
         # self.coco.showAnns(coco_annos,draw_bbox=True)
         num_objs = len(coco_annos)
         # Bounding boxes for objects
-        # In coco format, bbox = [xmin, ymin, width, height]
-        # In pytorch, the input should be [xmin, ymin, xmax, ymax]
         boxes = []
-        for i in range(num_objs):
-            xmin = coco_annos[i]['bbox'][0]
-            ymin = coco_annos[i]['bbox'][1]
-            xmax = xmin + coco_annos[i]['bbox'][2]
-            ymax = ymin + coco_annos[i]['bbox'][3]
-            boxes.append([xmin, ymin, xmax, ymax])
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        labels = torch.ones((num_objs,), dtype=torch.int64)
         # Tensorise img_id
         img_id = torch.tensor([img_id])
         # Size of bbox (Rectangular)
         areas = []
         coordinates = []
-        for i in range(num_objs):
-            areas.append(coco_annos[i]['area'])
+        categories = []
+        for item in coco_annos:
+            # In coco format, bbox = [xmin, ymin, width, height]
+            # In pytorch, the input should be [xmin, ymin, xmax, ymax]
+            xmin = item['bbox'][0]
+            ymin = item['bbox'][1]
+            xmax = xmin + item['bbox'][2]
+            ymax = ymin + item['bbox'][3]
+            boxes.append([xmin, ymin, xmax, ymax])
+
+            areas.append(item['area'])
             coordinate3d = []
-            for axis in zip('X', 'Y', 'Z'):
-                coordinate3d.append(coco_annos[i]['attributes'][f'coordination_{axis}'])
+            for axis in ['X', 'Y', 'Z']:
+                coordinate3d.append(item['attributes'][f'coordination_{axis}'])
             coordinates.append(coordinate3d) 
+            categories.append(item['category_id'])
+
+        categories = torch.as_tensor(categories, dtype=torch.int32)
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
         areas = torch.as_tensor(areas, dtype=torch.float32)
-        coordinates = torch.as_tensor(coordinates, dtype=torch.float32)
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        coordinates = torch.as_tensor(coordinates, dtype=torch.float64)
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int32)
 
         # Annotation is in dictionary format
         cater_annotation = {}
         cater_annotation["boxes"] = boxes
-        cater_annotation["labels"] = labels
+        cater_annotation["category_ids"] = categories
         cater_annotation["image_id"] = img_id
         cater_annotation["area"] = areas
         cater_annotation["iscrowd"] = iscrowd
@@ -83,5 +85,5 @@ if __name__ == '__main__':
     image_dir = os.path.join('images', 'image')
     annotations = os.path.join('annotations', '5200-5214.json')
     cdl = CaterDataloader(root, image_dir, annotations)
-    cdl.__getitem__(0)
+    print(cdl.__getitem__(0))
 
