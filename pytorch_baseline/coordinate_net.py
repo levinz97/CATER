a1be8d7d925df70fc4654c804bcdcb1e3076e5d9
Alaric <alarictong@gmail.com>
2022-01-22 19:09:09 +0100

    check model, still need modify

--- pytorch_baseline/coordinate_net.py
+++ pytorch_baseline/coordinate_net.py
@@ -25,12 +25,12 @@ class CoordinateNet():
             root = os.path.join('.', 'dataset')
 
         image_dir = os.path.join('images', 'image')
-        annotations = os.path.join('annotations', '5200-5214.json')
+        annotations = os.path.join('annotations', '5301-5305.json')
         self.dataloader = CaterDataloader(root, image_dir, annotations)
 
 
     def train_net(self, model, device):
-        num_epochs = 150
+        num_epochs = 2
         model.to(device)
         loss_mse = nn.MSELoss()
         loss_mse.to(device)
@@ -40,14 +40,24 @@ class CoordinateNet():
             model.train()
             i = 0
             for imgs, annotations in self.dataloader:
-                print(imgs.shape)
-                i+=1
-                imgs = list(img.to(device) for img in imgs)
-                bboxes = list({k: v.to(device)} for k, v in annotations if k =='boxes')
-                coordinates = list({k: v.to(device)} for k, v in annotations if k =='coordinates')
+                loss_image = 0
+                # print("-------------------")
+                # print(imgs)
+                # print(imgs.shape)
+                # print(type(imgs))
+                # transform1 = torchvision.transforms.ToPILImage()
+                # a = transform1(imgs)
+                # a.show()
+                # print(annotations)
+                # break
+                i += 1
+                imgs = imgs.to(device)
+                bboxes = annotations['boxes'].to(device)
+                coordinates = annotations['coordinates'].to(device)
                 for j in range(len(bboxes)):
                     output = model(imgs, bboxes[j])
                     loss = loss_mse(output, coordinates[j])
+                    loss_image += loss
 
                     # pred_bbox_d2 = self.detectron(imgs)
                     # loss = model(imgs, annotations, pred_bbox_d2)
@@ -55,8 +65,8 @@ class CoordinateNet():
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
-
-            print(f'Iteration: {i}/{len(self.dataloader)}, Loss:{loss}')
+                loss_image = loss_image / len(bboxes)
+                print(f'Iteration: {i}/{len(self.dataloader)}, Loss:{loss_image}')
 
 
 class Coordinate_model(nn.Module):
@@ -65,25 +75,30 @@ class Coordinate_model(nn.Module):
         self.data_transforms = transforms.Compose([
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                # transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406, 0.485, 0.456, 0.406], [0.229, 0.224, 0.225, 0.229, 0.224, 0.225])
             ])
+        self.resnet_model = torchvision.models.resnet50(pretrained=True)
+        num_ftrs = self.resnet_model.fc.in_features
+        self.resnet_model.conv1 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet_model.fc = nn.Linear(num_ftrs, 3)
+        # print(self.resnet_model)
 
-    def forward(self, img, pred_bbox_d2): #
+    def forward(self, img, pred_bbox_d2):
+        reshape_img = torch.reshape(img, (-1, 3, 240, 320))
         idx = torch.tensor([0])
+        idx = idx.to('cuda')
         pred_bbox_d2 = torch.cat((idx, pred_bbox_d2), dim=0)
         pred_bbox_d2 = torch.reshape(pred_bbox_d2, (-1, 5))
-        roi = torchvision.ops.roi_align(img, pred_bbox_d2, output_size=(240, 320), spatial_scale=1.0, sampling_ratio=-1)
-        new_img = torch.cat((img, roi), dim=1)
+        roi = torchvision.ops.roi_align(reshape_img, pred_bbox_d2, output_size=(240, 320), spatial_scale=1.0, sampling_ratio=-1)
+        new_img = torch.cat((reshape_img, roi), dim=1)
         output = self.data_transforms(new_img)
-        output = resnet_model(output)
+        output = self.resnet_model(output)
         return output
 
 
 if __name__ == '__main__':
-    resnet_model = torchvision.models.resnet50(pretrained=True)
-    num_ftrs = resnet_model.fc.in_features
-    resnet_model.fc = nn.Linear(num_ftrs, 3)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     coordinatenet = CoordinateNet()
-    coordinatenet.train_net(resnet_model, device)
+    coordinate_model = Coordinate_model()
+    coordinatenet.train_net(coordinate_model, device)
