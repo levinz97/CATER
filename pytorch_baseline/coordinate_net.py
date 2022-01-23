8eaabeaeb1b76ae517d43c2c0f50ba7529a875b2
Alaric <alarictong@gmail.com>
2022-01-23 12:43:30 +0100

    stack the boxes within the same image

--- pytorch_baseline/coordinate_net.py
+++ pytorch_baseline/coordinate_net.py
@@ -5,6 +5,7 @@ from torch import nn
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 import torch
+from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
 from dataloader import CaterDataloader
@@ -27,7 +28,7 @@ class CoordinateNet():
         image_dir = os.path.join('images', 'image')
         annotations = os.path.join('annotations', '5301-5305.json')
         self.dataloader = CaterDataloader(root, image_dir, annotations)
-
+        self.writer = SummaryWriter("coordinate_loss")
 
     def train_net(self, model, device):
         num_epochs = 50
@@ -41,60 +42,32 @@ class CoordinateNet():
             i = 0
             for imgs, annotations in self.dataloader:
                 loss_image = 0
-                # print("-------------------")
-                # print(imgs)
-                # print(imgs.shape)
-                # print(type(imgs))
-                # transform1 = torchvision.transforms.ToPILImage()
-                # a = transform1(imgs)
-                # a.show()
-                # print(annotations)
-                # break
                 i += 1
                 imgs = imgs.to(device)
-                bboxes = annotations['boxes'].to(device)
                 coordinates = annotations['coordinates'].to(device)
-                for j in range(len(bboxes)):
-                    output = model(imgs, bboxes[j])
-                    b = coordinates[j]
-                    b = torch.reshape(b, (-1, 3))
-                    loss = loss_mse(output, b)
-                    loss_image += loss
-                    # pred_bbox_d2 = self.detectron(imgs)
-                    # loss = model(imgs, annotations, pred_bbox_d2)
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-                loss_image = loss_image / len(bboxes)
-                print(f'Iteration: {i}/{len(self.dataloader)}, Loss:{loss_image}')
+                output = model(imgs)
+                loss = loss_mse(output, coordinates)
+                # pred_bbox_d2 = self.detectron(imgs)
+                # loss = model(imgs, annotations, pred_bbox_d2)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                print(f'Iteration: {i}/{len(self.dataloader)}, Loss:{loss}')
 
 
 class Coordinate_model(nn.Module):
     def __init__(self):
         super(Coordinate_model, self).__init__()
-        self.data_transforms = transforms.Compose([
-                transforms.Resize((224,224)),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ])
         self.resnet_model = torchvision.models.resnet50(pretrained=True)
         num_ftrs = self.resnet_model.fc.in_features
         self.resnet_model.conv1 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.resnet_model.fc = nn.Linear(num_ftrs, 3)
         # print(self.resnet_model)
 
-    def forward(self, img, pred_bbox_d2):
-        reshape_img = torch.reshape(img, (-1, 3, 240, 320))
-        idx = torch.tensor([0])
-        idx = idx.to('cuda')
-        pred_bbox_d2 = torch.cat((idx, pred_bbox_d2), dim=0)
-        pred_bbox_d2 = torch.reshape(pred_bbox_d2, (-1, 5))
-        roi = torchvision.ops.roi_align(reshape_img, pred_bbox_d2, output_size=(224, 224), spatial_scale=1.0, sampling_ratio=-1)
-        reshape_img = self.data_transforms(reshape_img)
-        output = torch.cat((reshape_img, roi), dim=1)
-        output = self.resnet_model(output)
+    def forward(self, img):
+        output = self.resnet_model(img)
         # print(output.shape)
         return output
-
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
