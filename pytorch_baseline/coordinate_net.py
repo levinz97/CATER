9d978065f15bb9455f770f40a86ea5f946acfaf3
Alaric <alarictong@gmail.com>
2022-01-24 18:11:15 +0100

    refactorcode add test part

--- pytorch_baseline/coordinate_net.py
+++ pytorch_baseline/coordinate_net.py
@@ -5,6 +5,7 @@ from torch import nn
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 import torch
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
@@ -26,25 +27,34 @@ class CoordinateNet():
             root = os.path.join('.', 'dataset')
 
         image_dir = os.path.join('images', 'image')
-        annotations = os.path.join('annotations', '5301-5305.json')
-        self.dataloader = CaterDataloader(root, image_dir, annotations)
-        self.writer = SummaryWriter("coordinate_loss")
+        train_annotations = os.path.join('annotations', 'train_dataset.json')
+        test_annotations = os.path.join('annotations', 'test_dataset.json')
+        train_batch_size = 6
+        test_batch_size = 1
+        self.train_dataset = CaterDataloader(root, image_dir, train_annotations)
+        self.train_dataloader = DataLoader(dataset=self.train_dataset, batch_size=train_batch_size, collate_fn=collate)
+        self.test_dataset = CaterDataloader(root, image_dir, test_annotations)
+        self.test_dataloader = DataLoader(dataset=self.test_dataset, batch_size=test_batch_size, collate_fn=collate)
+        self.writer = SummaryWriter("coordinate_loss_{}".format(train_batch_size))
 
     def train_net(self, model, device):
-        num_epochs = 50
+        num_epochs = 20
         model.to(device)
         loss_mse = nn.MSELoss()
         loss_mse.to(device)
         optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0001)
 
+        total_train_step = 0
         for epoch in range(num_epochs):
+            print("------------------{} epoch start -------------------".format(epoch+1))
             model.train()
-            i = 0
-            for imgs, annotations in self.dataloader:
-                loss_image = 0
-                i += 1
+            for data in self.train_dataloader:
+                imgs, coordinates = data
+                # print(imgs.shape)
+                # print(coordinates.shape)
                 imgs = imgs.to(device)
-                coordinates = annotations['coordinates'].to(device)
+                coordinates = coordinates.to(device)
+                # print(coordinates.shape)
                 output = model(imgs)
                 loss = loss_mse(output, coordinates)
                 # pred_bbox_d2 = self.detectron(imgs)
@@ -52,8 +62,49 @@ class CoordinateNet():
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                print(f'Iteration: {i}/{len(self.dataloader)}, Loss:{loss}')
 
+                total_train_step += 1
+                if total_train_step % 50 == 0:
+                    print("训练次数：{}, Loss:{}".format(total_train_step, loss.item()))
+                    self.writer.add_scalar("train_loss", loss.item(), total_train_step)
+            torch.save(model, "./model_output/coord_model_{}.pth".format(epoch))
+
+    def test(self, model, device):
+        model.to(device)
+        loss_mse = nn.MSELoss()
+        loss_mse.to(device)
+        model.eval()
+        total_test_loss = 0
+        step = 0
+        with torch.no_grad():
+            for data in self.test_dataloader:
+                imgs, coordinates = data
+                # print(imgs.shape)
+                # print(coordinates.shape)
+                imgs = imgs.to(device)
+                coordinates = coordinates.to(device)
+                # print(coordinates.shape)
+                output = model(imgs)
+                loss = loss_mse(output, coordinates)
+                # pred_bbox_d2 = self.detectron(imgs)
+                # loss = model(imgs, annotations, pred_bbox_d2)
+                total_test_loss += loss
+                step += 1
+                if step % 5 == 0:
+                    print(step)
+            print(total_test_loss.item())
+            average_loss = total_test_loss / step
+            print(average_loss.item())
+
+def collate(batch):
+    list1 = []
+    list2 = []
+    for data in batch:
+        list1.append(data[0])
+        list2.append(data[1])
+        imgs = torch.cat(list1, dim=0)
+        labels = torch.cat(list2, dim=0)
+    return imgs, labels
 
 class Coordinate_model(nn.Module):
     def __init__(self):
@@ -69,8 +120,15 @@ class Coordinate_model(nn.Module):
         # print(output.shape)
         return output
 
+
 if __name__ == '__main__':
+    train = False
+    test = True
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_model = torch.load("coord_model_18.pth") #load the trained model
     coordinatenet = CoordinateNet()
     coordinate_model = Coordinate_model()
-    coordinatenet.train_net(coordinate_model, device)
+    if train:
+        coordinatenet.train_net(coordinate_model, device)
+    if test:
+        coordinatenet.test(test_model, device)
