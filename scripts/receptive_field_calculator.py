03a3830cfaa628a61fe4b5465a8c9c567ef2719d
levinz97 <levinzeng97@gmail.com>
2022-02-08 00:13:42 +0100

    update receptive field calculator

--- scripts/receptive_field_calculator.py
+++ scripts/receptive_field_calculator.py
@@ -7,7 +7,7 @@ def calculate_rf(kernel_size, dilations, stride, n_layers):
     kernel_size = (kernel_size-1)*dilations[0] + 1
     if isinstance(stride, int):
         stride = np.repeat(stride, n_layers)
-        n_layers = len(dilations)
+    n_layers = len(dilations)
     cumprod_stride = np.cumprod(stride)
     print(f'cumprod_stride={cumprod_stride}')
     rf = kernel_size
@@ -30,5 +30,5 @@ if __name__ == '__main__':
     
     print(calculate_rf(kernel_size=3, dilations=[3,3,3], stride=2, n_layers=n_layers))
     print(calculate_rf(kernel_size=3, dilations=[2,1,1], stride=1, n_layers=n_layers))
-    print(calculate_rf(kernel_size=3, dilations=[2,2,2,2,2], stride=2, n_layers=n_layers))
+    print(calculate_rf(kernel_size=3, dilations=[24,24,18,18,18], stride=[2,2,2,2,1], n_layers=n_layers))
 
